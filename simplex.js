7d3a58c9c93894e9a635d091f01704836422a53e
aj heller <aj@drfloob.com>
2013-05-30 14:46:59 -0700

    about to reimplement setup

--- simplex.js
+++ simplex.js
@@ -1,6 +1,8 @@
 function SimplexModule(stdlib, foreign, heap) {
     "use asm";
 
+    var sqrt = stdlib.Math.sqrt;
+
     /**
      * Solves a simplex optimization problem presented in basic feasible solved form.
      * 
@@ -12,29 +14,50 @@ function SimplexModule(stdlib, foreign, heap) {
      *     the first is treated as a static constant, the rest coefficients
      * 
      */
-    function simplex_opt(i_C, i_f) {
-	var f= Float32Array(i_f);
-	var C= Float32Array(i_C);
-	var varcnt= (f.length-1)|0;
-	var rowcnt= (C.length/f.length)|0;
-	
-	var i= 0|0, j=0|0, J= 0|0;
+    function simplex_opt(heap) {
+	heap = 
+	var varcnt= 0;
+	varcnt= (sqrt(
+	    heap.byteLength 
+		/ 
+		4.0)
+		 -1)|0;
 
-	for(i=0|0; i< varcnt; i=(i+1)|0) {
-	    if (f[i+1] < 0) {
-		J=(i+1)|0;
-		for(j=0|0; j<rowcnt; j=(j+1)|0) {
-		    if (C[j+J] < 0) {
-			
-		    }
-		    // if none, then no solution
-		}
-	    }
-	    // if none, then solved
-	}
+	var f= Float32Array(heap, 0, (varcnt+1)|0);
+	var C= Float32Array(heap, ((varcnt+1)*4)|0);
+	var rowcnt= (varcnt-1)|0;
+	
+	var i= 0|0, j=0|0;
+
+	for(i=0|0; i< varcnt; i=(i+1)|0)
+	    if (f[i+1] < 0)
+		break;
+	if (i == varcnt)
+	    return 1|0;
+	i= (i+1)|0;
+
+	for(j=0|0; j<rowcnt; j=(j+1)|0)
+	    if (C[j+i] < 0)
+		break;
+	if (j == rowcnt)
+	    return 0|0;
+
+	return 2|0; // not done
     };
 
     function simplex_solve() {};
 
-    return { solve: simplex_solve };
+    return { solve: simplex_opt };
+}
+
+
+SimplexModule.setup = function(f, C) {
+    var sizeMult = (f.length/32)|0;
+    var heap = new ArrayBuffer(4096*sizeMult);
+    var h = new Float32Array(heap);
+    h[0] = f.length;
+    h.set(f, 1);
+    for(var i=0; i<C.length; i++)
+	h.set(C[i], (f.length+1)*(i+1));
+    return heap;
 }
