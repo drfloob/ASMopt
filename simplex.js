e94aca2abf9dd1ac6b064b731cccab2764006a12
aj heller <aj@drfloob.com>
2013-05-30 17:11:06 -0700

    everything appears to work up through the choice of exit variable

--- simplex.js
+++ simplex.js
@@ -1,10 +1,40 @@
+// rows & variables are ZERO-INDEXED
+
+
 function SimplexModule(stdlib, foreign, heap) {
     "use asm";
 
     var sqrt = stdlib.Math.sqrt;
+    var imul = stdlib.Math.imul;
     var HEAP32F = new stdlib.Float32Array(heap);
     var HEAP32I = new stdlib.Int32Array(heap);
-    
+
+    console.log(HEAP32F);
+    function getf(i) {
+	i= i|0;
+	return +HEAP32F[2+i << 2 >> 2];
+    }
+
+    function getc(i, rowlen) {
+	i= i|0; rowlen= rowlen|0;
+	// console.log("getc", 2+rowlen+i);
+	return +HEAP32F[2+rowlen+i << 2 >> 2];
+    }
+
+    function getrc(r, i, rowlen) {
+	r= r|0; 
+	i= i|0; 
+	rowlen= rowlen|0;
+	return +getc(imul(r, rowlen) + i|0, rowlen);
+    }
+
+    function getrb(r, rowlen) {
+	r= r|0; rowlen= rowlen|0;
+	return +getc(imul(r, rowlen)-1|0, rowlen);
+    }
+
+
+
 
     /**
      * Solves a simplex optimization problem presented in basic feasible solved form.
@@ -18,24 +48,48 @@ function SimplexModule(stdlib, foreign, heap) {
      * 
      */
     function simplex_opt() {
-	var varcnt= 0, rowcnt= 0, i= 0, j=0, foff=0;
+	var varcnt= 0, rowcnt= 0, rowlen= 0;
+	var i= 0, j=0;
+	var exitVal= 999999999.0, exitIdx= 0, aij= 0.0, bi= 0.0, tmp= 0.0;
 
 	varcnt= (HEAP32I[0])|0;
-	rowcnt= (varcnt-1)|0;
+	rowcnt= (varcnt)|0;
+	rowlen= (varcnt+1)|0;
 
-	for(; (i|0) < (varcnt|0); i=(i+1)|0)
-	    if (+HEAP32F[(i+2) >> 2] < +0)
+	for(; (i|0) < (varcnt|0); i=(i+1)|0) {
+	    if (+getf(i|0) < +0)
 		break;
+	}
 	if ((i|0) == (varcnt|0))
-	    return 1;
-	i= (i+1)|0;
+	    return 0;
+	console.log("entry variable", i);
 
-	foff = (varcnt+1)|0;
-	for(; (j|0) < (rowcnt|0); j=(j+1)|0)
-	    if (+HEAP32F[j+foff >> 2] < +0)
+	for(; (j|0) < (rowcnt|0); j=(j+1)|0) {
+	    aij = +getrc(j, i, rowlen);
+	    // console.log(j, i, aij);
+	    if (aij < +0)
 		break;
+	}
 	if ((j|0) == (rowcnt|0))
-	    return 0|0;
+	    return 1;
+
+
+	for(j=0; (j|0) < (rowcnt|0); j=(j+1)|0) {
+	    // aij = +getf(imul(j+1, rowlen) + i + 1|0);
+	    aij = getrc(j, i, rowlen);
+	    console.log("j (row)", j, "aij", aij);
+	    if (+aij < +0) {
+		bi = +getrb(j, rowlen);
+		console.log(j, " bi: ", bi);
+		tmp = +((-1.0 * +bi) / +aij);
+		console.log("aij < 0 at", j, "aij: ", aij, "-b/aij: ", tmp);
+		if (+tmp < +exitVal) {
+		    exitVal= +tmp;
+		    exitIdx= j|0;
+		}
+	    }
+	}
+	console.log("exit variable", exitIdx);
 
 	return 2|0; // not done
     };
@@ -50,15 +104,12 @@ SimplexModule.setup = function(f, C) {
     var sizeMult = (f.length)|0;
     var minByteSize = Math.max(4096, Math.pow(f.length, 2) * 32 / 8);
     var mult = Math.ceil(Math.log(minByteSize)/Math.log(2));
-
-    // console.log(sizeMult, minByteSize, mult);
-
     var heap = new ArrayBuffer(Math.pow(2, mult));
     var h = new Float32Array(heap);
     var hi = new Int32Array(heap);
-    hi[0] = f.length;
+    hi[0] = f.length-1;
     h.set(f, 1);
     for(var i=0; i<C.length; i++)
-	h.set(C[i], (f.length+1)*(i+1));
+	h.set(C[i], (f.length)*(i+1) + 1);
     return heap;
 }
